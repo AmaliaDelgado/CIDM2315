2b739f8c1d1f10ff078b6abdd4f21e3e2e81e2d5
AmaliaDelgado <ajdelgadoruiz1@buffs.wtamu.edu>
2026-01-26 17:14:57 -0600

    Submit Homework1

---
+++ Homework1CIDM.cs
@@ -0,0 +1,6 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");//
+double x=2.5;
+double y=3.3;
+double Z=4*x*x+3*y;
+Console.WriteLine("the value of z is:"+Z);
